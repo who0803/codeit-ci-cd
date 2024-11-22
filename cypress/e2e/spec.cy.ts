23c9d36093d913b5bba6f8cac5cae387a635d018
who0803 <10rlatkdtn@naver.com>
2024-11-22 15:48:03 +0900

    feat: Modify text

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -10,9 +10,9 @@ describe("template spec", () => {
   it('should contain "Get started by ~" text', () => {
     cy.contains("Get started by editing");
   });
-  it('should contain "Docs" heading anchor with correct href prop', () => {
-    cy.contains("Docs")
+  it('should contain "Learn" heading anchor with correct href prop', () => {
+    cy.contains("Learn")
       .should("have.attr", "href")
-      .and("include", "https://nextjs.org/docs");
+      .and("include", "https://nextjs.org/learn");
   });
 });
